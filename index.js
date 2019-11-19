0698c64d0960c5ee21c69c6028c0063157d7dfe4
Grant Hewitt <grant_hewitt_1@hotmail.co.uk>
2019-11-19 17:56:23 +0100

    Add initial test apollo-server implementation

---
+++ index.js
@@ -0,0 +1,76 @@
+const { ApolloServer, gql } = require('apollo-server');
+const https = require('https');
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class ConferencesApi extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'http://localhost:3000/';
+  }
+
+  async getConference(uniqueName) {
+    return this.get(`conferences/${uniqueName}`);
+  }
+
+  async getConferences() {
+    return this.get(`conferences`);
+  }
+}
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Conference {
+    uniqueName: String
+    displayName: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    conferences: [Conference]
+  }
+`;
+
+const conferences = [
+  {
+    "uniqueName": "test-conf-1",
+    "displayName": "Test Conference One"
+  },
+  {
+    "uniqueName": "test-conf-2",
+    "displayName": "Test Conference Two"
+  }
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    conferences: (_source, { id }, { dataSources }) => {
+      return dataSources.conferencesApi.getConferences();
+    },
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      conferencesApi: new ConferencesApi()
+    }
+  }
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});

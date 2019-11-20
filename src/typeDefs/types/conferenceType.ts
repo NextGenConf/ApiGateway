3bde99456dfebf024989c4e0a21f8a45a59a13f3
Grant Hewitt <ghewitt@users.noreply.github.com>
2019-11-20 18:41:53 +0100

    Update Conference model to match API (#2)

--- src/typeDefs/types/conferenceType.ts
+++ src/typeDefs/types/conferenceType.ts
@@ -1,8 +1,26 @@
 import { gql } from 'apollo-server';
 
 export default gql`
+  type Address {
+    addresLine: String
+    city: String
+    country: String
+    postalCode: String
+  }
+
+  type Venue {
+    name: String
+    address: Address
+  }
+
   type Conference {
     uniqueName: String
     displayName: String
+    iconUri: String
+    description: String
+    subtitle: String
+    startDate: String
+    endDate: String
+    venue: Venue
   }
 `;

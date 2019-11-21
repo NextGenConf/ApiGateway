9f0f535fbe1bfefc572348985e08a4b2b76792a5
Grant Hewitt <ghewitt@users.noreply.github.com>
2019-11-21 03:07:22 +0100

    Add query for getting conference by uniqueName (#4)

--- src/resolvers/conferenceResolvers.ts
+++ src/resolvers/conferenceResolvers.ts
@@ -1,7 +1,10 @@
 export default {
   Query: {
-    conferences: (_source: any, _: any, { dataSources }: any) => {
+    getConferences: (_parent: any, _args: any, { dataSources }: any) => {
       return dataSources.conferencesApi.getConferences();
     },
+    getConference: (_parent: any, args: any, { dataSources }: any) => {
+      return dataSources.conferencesApi.getConference(args.uniqueName);
+    }
   },
 };
--- src/typeDefs/query.ts
+++ src/typeDefs/query.ts
@@ -2,6 +2,7 @@ import { gql } from 'apollo-server';
 
 export default gql`
   type Query {
-    conferences: [Conference]
+    getConferences: [Conference]
+    getConference(uniqueName: String): Conference
   }
 `;
